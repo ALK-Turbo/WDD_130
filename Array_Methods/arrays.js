3b6a2c9ad810b409b647dc9c603cce3fced35744
ALK-Turbo <kel16018@byui.edu>
2022-10-20 08:22:21 -0600

    commit

--- Array_Methods/arrays.js
+++ Array_Methods/arrays.js
@@ -15,3 +15,45 @@ console.log(html)
 myList.innerHTML = html
 
 console.log(htmlStrings)
+
+const lettergrades = ['A', 'B', 'A'];
+function getGPA(grade) {
+    let points = 0
+    switch (grade) {
+        case 'A':
+            points = 4;
+            break;
+        case 'B':
+            points = 3;
+            break;
+        case 'C':
+            points = 2;
+            break;
+        default:
+            points = 1;
+            break;
+    }
+    return points
+}
+const my_grade = lettergrades.map(getGPA);
+console.log(my_grade);
+
+// example 2
+const grades = ["A", "B", "A"];
+function convertGradeToPoints(grade) {
+    let points = 0;
+    if (grade === "A") {
+        points = 4;
+    } else if (grade === "B") {
+        points = 3;
+    }
+    return points;
+}
+const gpaPoints = grades.map(convertGradeToPoints);
+console.log(gpaPoints)
+
+function sumPoints(total, current) {
+    return total + current;
+}
+const averageGPA = gpaPoints.reduce(sumPoints, 0) / gpaPoints.length
+console.log(averageGPA.toFixed(2))
