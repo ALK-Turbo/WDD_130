99c08dbd36886ab0ebe3ff98948c53a01e44916a
ALK-Turbo <kel16018@byui.edu>
2022-10-20 08:44:47 -0600

    commit

--- Array_Methods/arrays.js
+++ Array_Methods/arrays.js
@@ -58,8 +58,8 @@ function sumPoints(total, current) {
 const averageGPA = gpaPoints.reduce(sumPoints, 0) / gpaPoints.length
 console.log(averageGPA.toFixed(2))
 //try this one again, it is jjust like the one above, just in one line instead of several
-// const averageGPA2 = gpaPoints((total, current) => total + current, 0);
-// console.log(averageGPA2)
+const averageGPA2 = gpaPoints.reduce((total, current) => total + current, 0) / gpaPoints.length;
+console.log(averageGPA2.toFixed(2))
 
 
 const fruits = ['watermelon', 'peach', 'apple', 'tomato', 'grape'];
@@ -71,3 +71,7 @@ const shortWords = fruits2.filter(function (word) {
     return word.length < 6;
 });
 console.log(shortWords);
+
+const numbers = [12, 34, 21, 54]
+const lucky_number = 21
+console.log(numbers.indexOf(lucky_number))
