3179f0bd826f9a67aedfd3129ae75fb3e19b6acb
ALK-Turbo <kel16018@byui.edu>
2022-11-04 08:52:30 -0600

    commit

--- Register/register.js
+++ Register/register.js
@@ -82,3 +82,14 @@ form.addEventListener('click', function onClick(event) {
     submitForm();
     totalFees();
 });
+
+
+const checkbox = document.querySelector("#add");
+
+checkbox.addEventListener("click", checkboxClick, false);
+
+function checkboxClick(event) {
+    let warn = "preventDefault() won't let you check this!<br>";
+    document.getElementById("output-box").innerHTML += warn;
+    event.preventDefault();
+}
