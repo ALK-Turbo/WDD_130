0d88a93c5775c7ba90901fa773e601904a6fd90b
ALK-Turbo <kel16018@byui.edu>
2022-11-12 22:17:59 -0700

    commit

--- apod/apod.js
+++ apod/apod.js
@@ -13,10 +13,17 @@ async function getApod(url, callBack) {
 function doStuff(data) {
     const output = document.querySelector("#output");
     photo = data
-    const html = `<img src="${photo.url}" alt="Source Image">`;
-    output.insertAdjacentHTML("afterbegin", html);
+    const html = `<section class="information"><img src="${photo.url}" alt="${photo.title}">
+    <div class="info">
+        <h2>${photo.title}</h2>
+        <h3>${photo.date}</h3>
+        <p>${photo.explanation}</p>
+    </div>
+    </section>
+    `;
+    output.insertAdjacentHTML("beforeend", html);
     console.log("first");
 };
 
-console.log(getApod(apodUrl, doStuff));
+// console.log(getApod(apodUrl, doStuff));
 getApod(apodUrl, doStuff)
